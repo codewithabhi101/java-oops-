c0bd0d2868d251af41f22ca28307aef7a5552f95
ABHI123RR <abhirocks.benachigere2005@gmail.com>
2025-08-28 16:22:30 +0530

    array

---
+++ demo5.java
@@ -0,0 +1,15 @@
+public class demo5{
+    public static void main(String[] args)
+    {
+       int nums[] = new int[4]; 
+       nums[0] = 4;
+       nums[1] = 8;
+       nums[2] = 3;
+       nums[3] = 9;
+
+       for(int i =0;i<4;i++)
+       {
+        System.out.println(nums[i]);
+       }
+    }
+}
