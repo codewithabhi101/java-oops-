987549c3df6f423041a1a4467808f278eb78d283
ABHI123RR <abhirocks.benachigere2005@gmail.com>
2025-08-29 22:04:17 +0530

    string

---
+++ Demo7.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Demo7 {
+    public static void main(String [] args){
+    try (Scanner sc = new Scanner(System.in)){
+        System.out.println("Enter the first number : ");
+        if (num >= 0){
+        System.out.prinln("Number is positive.");
+      }else{
+      System.out.prinln("The number is negative.");
+      }
+}
+    }
